8aa205543b8ee3bba397807b3df9b6766ead84bc
naufalnab <naufalnabila27@gmail.com>
2026-09-25 09:15:37 +0700

    feat: add team-members module

--- src/modules/team-members/team-members.module.ts
+++ src/modules/team-members/team-members.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AssetsModule } from '#src/modules/assets/assets.module';
 import { AuditModule } from '#src/modules/audit/audit.module';
+import { DrizzleModule } from '#src/modules/drizzle/drizzle.module';
 import { TeamMembersController } from './team-members.controller';
 import { TeamMembersRepository } from './team-members.repository';
 import { TeamMembersService } from './team-members.service';
 
 @Module({
-  imports: [AssetsModule, AuditModule],
+  imports: [DrizzleModule, AssetsModule, AuditModule],
   controllers: [TeamMembersController],
   providers: [TeamMembersRepository, TeamMembersService],
   exports: [TeamMembersService, TeamMembersRepository],
